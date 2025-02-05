5232efe23e8087004e08bcc1717e22ed8690fca8
Ann Zabolotskikh <ann.zblt@yandex.ru>
2025-02-05 19:15:41 +0300

    Add styles and markup for PW

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: "../images/arhyz.jpeg",
+  },
+  {
+    name: "Челябинская область",
+    link: "../images/chelyabinckaya-oblast.jpeg",
+  },
+  {
+    name: "Иваново",
+    link: "../images/ivanovo.jpeg",
+  },
+  {
+    name: "Камчатка",
+    link: "../images/kamchatka.jpeg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "../images/holmogosky-rayon.jpeg",
+  },
+  {
+    name: "Байкал",
+    link: "../images/baikal.jpeg",
+  }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+
+const createCard = (item, removeCardCb) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__title').textContent = item.name;
+
+  deleteButton.addEventListener('click', () => {
+    removeCardCb(cardElement);
+  });
+
+  placesList.append(cardElement);
+}
+
+const deleteCard = (card) => {
+  card.remove();
+}
+
+initialCards.forEach((item) => {
+  createCard(item, deleteCard);
+});
